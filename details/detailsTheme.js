025ab2021c20dc3a798d070c5ff70e83d9aaed11
MonjedNidal <monjed1010@gmail.com>
2023-11-19 15:35:49 +0200

    details page done

--- details/detailsTheme.js
+++ details/detailsTheme.js
@@ -1,2 +1,26 @@
+const darkModeButton = document.querySelectorAll(".navListButton")[0];
 const body = document.body;
+const theme = window.localStorage.getItem("theme");
 
+const setDarkTheme = () => {
+  body.classList.add("darkDetails");
+  localStorage.setItem("theme", "dark");
+};
+const setLightTheme = () => {
+  body.classList.remove("darkDetails");
+  localStorage.setItem("theme", "light");
+};
+
+darkModeButton.addEventListener("click", () => {
+  if (localStorage.getItem("theme") === "dark") {
+    setLightTheme();
+  } else {
+    setDarkTheme();
+  }
+});
+
+if (localStorage.getItem("theme") === "dark") {
+  setDarkTheme();
+} else {
+  localStorage.setItem("theme", "light");
+}
